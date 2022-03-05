48f6b9dc12b589aa0809ab9196dca306097bc936
sarsdev <sarsdesenv@gmail.com>
2022-03-05 18:15:38 -0300

    Updating data details

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 var context;
 var widthWindow = (window.innerWidth - getCalcWidthTotalElements() > window.innerHeight - getCalcHeightTotalElements() ? window.innerHeight - getCalcHeightTotalElements() : window.innerWidth - getCalcWidthTotalElements()) * 0.8;
+widthWindow = widthWindow < 380 ? 380 : widthWindow;
 var coodsGeneral = {
     maxPositonX: widthWindow,
     maxPositonY: widthWindow,
@@ -10,7 +11,6 @@ var coodsGeneral = {
 };
 var spacesClickable = [];
 var currentPlayer = "X";
-var gameIsOver = false;
 var board = {
     space1: null,
     space2: null,
@@ -22,10 +22,19 @@ var board = {
     space8: null,
     space9: null
 };
+var turno = document.querySelector(".turno");
+var scorePlayerX = 0;
+var scoreX = document.querySelector("#placarX");
+var scorePlayerO = 0;
+var scoreO = document.querySelector("#placarO");
+var scoreDraw = 0;
+var draw = document.querySelector("#placarEmpate");
+var historic = document.querySelector("ul");
 
 (() => {
     window.addEventListener("resize", () => document.location.reload(true));
     inicializeCanvas();
+    inicializeDetails();
     createBoard();
 })();
 
@@ -38,7 +47,6 @@ function inicializeCanvas() {
 }
 
 function onClickCanvas(e) {
-    if(gameIsOver) { return; }
     spacesClickable.forEach((space) => {
         if(space.clicked(e.offsetX, e.offsetY)) {        
             if(board[`space${space.indexSpace}`] !== null) { return; }
@@ -47,14 +55,63 @@ function onClickCanvas(e) {
             let statusGame = isGameFinish();
             if(statusGame.finish) {
                 if(statusGame.winner !== null) {
-                    console.log(`O VENCEDOR É ${statusGame.winner}`);
+                    setScore(statusGame.winner);
                 } else {
-                    console.log(`DEU VELHA! EMPATOU :(`);
+                    setScore(null);
                 }
-                gameIsOver = true;
+                clearBoard();
             } else {
                 currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                turno.textContent = `Jogador ${currentPlayer}`;
+                turno.style.color = currentPlayer === 'X' ? 'blue' : 'red';
             }
         }
     });
+}
+
+function inicializeDetails() {
+    turno.textContent = `Jogador ${currentPlayer}`;
+    turno.style.color = 'blue';
+    scoreX.textContent = `Jogador X: ${scorePlayerX}`;
+    scoreO.textContent = `Jogador O: ${scorePlayerO}`;
+    draw.textContent = `Empates: ${scoreDraw}`;
+}
+
+function setScore(pWinner) {
+    if(pWinner === 'X') {
+        scorePlayerX++;
+        scoreX.textContent = `Jogador X: ${scorePlayerX}`;
+        let nodeLi = document.createElement("li");
+        nodeLi.appendChild(document.createTextNode("Jogador X ganhou"));
+        historic.appendChild(nodeLi);
+    } else if(pWinner === 'O') {
+        scorePlayerO++;
+        scoreO.textContent = `Jogador O: ${scorePlayerO}`;
+        let nodeLi = document.createElement("li");
+        nodeLi.appendChild(document.createTextNode("Jogador O ganhou"));
+        historic.appendChild(nodeLi);
+    } else {
+        scoreDraw++;
+        draw.textContent = `Empates: ${scoreDraw}`;
+        let nodeLi = document.createElement("li");
+        nodeLi.appendChild(document.createTextNode("Empate"));
+        historic.appendChild(nodeLi);
+    }
+}
+
+function clearBoard() {
+    board = {
+        space1: null,
+        space2: null,
+        space3: null,
+        space4: null,
+        space5: null,
+        space6: null,
+        space7: null,
+        space8: null,
+        space9: null
+    };
+    spacesClickable = [];
+    currentPlayer = "X";
+    createBoard();
 }
